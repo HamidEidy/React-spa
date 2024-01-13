da80d53ee47507e2b54beedd8089293c812d7a4d
HAM!DREZA <hamidreza.eidy1999@gmail.com>
2024-01-13 19:25:23 +0330

    create singlePage of Posts

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Users from './pages/Users';
 import ShowUser from './pages/Users/ShowUser';
 import Posts from './pages/Posts';
+import ShowPost from './pages/Posts/ShowPost';
 function App() {
   return (
     <BrowserRouter>
@@ -12,7 +13,8 @@ function App() {
       <Route path='/' element={<Home/>} />
       <Route path='/Users' element={<Users/>} />
       <Route path='/Users/:userId' element={<ShowUser />} />
-      <Route path='Posts' element={<Posts />} />
+      <Route path='/Posts' element={<Posts />} />
+      <Route path='Posts/:postId' element={<ShowPost />} />
     </Routes>
     </BrowserRouter>
 
---
+++ src/pages/Posts/ShowPost.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom"
+
+const ShowPost = () => {
+    const { postId } = useParams();
+    const [post, setPost] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+
+    useEffect(() => {
+
+        fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+            .then(res => res.json())
+            .then(data => {
+                setPost(data);
+                setLoading(false)
+                setError(null)
+            }).catch(err => {
+                setError(err.message)
+                setLoading(false)
+            })
+
+    }, [postId]);
+
+    return (
+    <div className="container ">
+        <div className="row">
+        {error && <div>{error}</div>}
+            {loading && <div className="spinner-border"></div>}
+            {post && <div className="col-md-8 mt-5">
+                <div className="card">
+                    <div className="card-header">
+                        {post.title}
+                    </div>
+                    <ul className="card-body">
+                        {post.body}
+                    </ul>
+                    <div className="card-footer">
+             
+                        <Link className="btn btn-sm btn-dark" to={`/posts/edit/${postId}`}>Edit</Link>
+                    </div>
+                </div>
+            </div>}
+        </div>
+    </div>
+
+    
+    )
+}
+
+export default ShowPost
