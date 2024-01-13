5f89e0902a3aefe938f57ed8fb1236b5333f3591
HAM!DREZA <hamidreza.eidy1999@gmail.com>
2024-01-13 11:40:01 +0330

    create home Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from './components/Header';
+import Home from './pages/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <BrowserRouter>
         <Header />
     <Routes>
-      <Route />
+      <Route path='/' element={<Home/>} />
     </Routes>
     </BrowserRouter>
 
